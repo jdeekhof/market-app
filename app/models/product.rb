337fb72d85587e65d33253ea7942bcc707268fce
Jake <jdeekhof@gmail.com>
2024-11-17 15:00:14 -0500

    preliminary model updates

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,5 @@ class Product < ApplicationRecord
   validates_presence_of :name, :category, :units_of_sale, :cents_price_per_unit
   validates_uniqueness_of :name
 
-
+  enum :units_of_sale,  [ :discrete, :per_gram, :per_pound, :per_ton ]
 end
