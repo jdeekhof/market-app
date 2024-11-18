f3d9e2074aebdea5660f8b8dd3bde0c3af794876
Jake <jdeekhof@gmail.com>
2024-11-18 13:08:22 -0500

    cart items start at 0

--- db/migrate/20241118013145_create_carts_and_cart_items.rb
+++ db/migrate/20241118013145_create_carts_and_cart_items.rb
@@ -8,7 +8,7 @@ class CreateCartsAndCartItems < ActiveRecord::Migration[8.0]
       t.references :cart, null: false, foreign_key: true
       t.references :product, null: false, foreign_key: true
       t.references :promotion, foreign_key: true
-      t.integer :quantity, null: false, default: 1
+      t.integer :quantity, null: false
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,7 @@ ActiveRecord::Schema[8.0].define(version: 2024_11_18_013145) do
     t.bigint "cart_id", null: false
     t.bigint "product_id", null: false
     t.bigint "promotion_id"
-    t.integer "quantity", default: 1, null: false
+    t.integer "quantity", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["cart_id"], name: "index_cart_items_on_cart_id"
